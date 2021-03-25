f5c04f26af0722d37629f461435bd6a46b83373f
pyf <koakumaping@163.com>
2021-03-25 16:20:04 +0800

    feat: 侧边栏样式升级

--- src/LeftSide/index.tsx
+++ src/LeftSide/index.tsx
@@ -1,10 +1,22 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { Layout } from 'antd'
+import { MenuFoldOutlined, MenuUnfoldOutlined } from '@ant-design/icons'
 import './index.sass'
 
 const ZyLeftSide: React.FC = props => {
+  const [ collapsed, setCollapsed ] = useState(false)
+  const toggleCollapsed = () => {
+    setCollapsed(!collapsed)
+  }
+
   return (
-    <Layout.Sider style={{ overflow: 'auto' }} width={208} collapsible={true}>
+    <Layout.Sider style={{ overflow: 'auto' }} width={208} trigger={null} collapsible collapsed={collapsed}>
+      {
+        React.createElement(collapsed ? MenuUnfoldOutlined : MenuFoldOutlined, {
+          className: 'zy-side-trigger',
+          onClick: toggleCollapsed,
+        })
+      }
       { props.children }
     </Layout.Sider>
   )
