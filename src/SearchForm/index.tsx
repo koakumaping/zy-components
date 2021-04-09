ae59f51342be4cff8a46c43c2c53249987888879
pyf <koakumaping@163.com>
2021-04-09 09:36:07 +0800

    feat: update

--- src/SearchForm/index.tsx
+++ src/SearchForm/index.tsx
@@ -54,6 +54,7 @@ const ZySearchForm = forwardRef((props: Props, ref) => {
     window.setTimeout(() => {
       if (formInstance) formInstance.resetFields()
     }, 80)
+    console.log('ZySearchForm' ,query)
     return query
   }, [history.location])
 
